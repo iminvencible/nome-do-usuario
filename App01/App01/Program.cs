f3a725c3b4369888e6a94d56a9691a05ae17c33c
iminvencible <167816974+iminvencible@users.noreply.github.com>
2025-03-19 18:50:21 -0300

    Add files via upload

---
+++ App01/App01/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using App01;
+
+Usuario user01 = new Usuario(); // Criação do objeto user01 do tipo Usuario
+user01.Nome = "Israel"; // Executa o método set do atributo nome
+user01.Email = "israel@lucania.com.br"; // Executa o método set do atributo email
+user01.Login = "israel@lucania.com.br"; // Executa o método set do atributo login
+user01.Senha = "*****"; // Executa o método set do atributo senha
+user01.provarExistencia(); // Executa o método provarExistencia
+
+Usuario user02 = new Usuario();
+user02.Nome = "Thalita";
+user02.Email = "Marcio@gmail.com";
+user02.provarExistenciaNmGmail();
+
+Usuario user03 = new Usuario();
+user03.Nome = "";
+user03.Email = "";
+user03.Login = "";
+user03.Senha = "";
+user03.pedirInformacaos();
+
---
+++ App01/App01/Usuario.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App01
+{
+
+    class Usuario()
+    {
+       
+        //Definição dos atributos como privados
+        private string nome;
+        private string email;
+        private string login;
+        private string senha;
+
+
+        //Definição dos getters e setters
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+        public string Login { get => login; set => login = value; }
+        public string Senha { get => senha; set => senha = value; }
+
+        //Definição do ou dos métodos da classe Usuario
+        public void provarExistencia()
+        {
+            Console.WriteLine("Olá, a classe funciona");
+            Console.WriteLine("As informações cadastradas foram:");
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("Login: " + this.Login);
+            Console.WriteLine("Senha: " + this.Senha);
+            Console.WriteLine("  ");
+
+        }
+
+        //adicionar um novo método que lista apenas o nome e e-mail do usuário
+        public void provarExistenciaNmGmail()
+        {
+            Console.WriteLine("Olá, aqui está sua informação");
+            Console.WriteLine("Aqui está seu nome e email:");
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("     ");
+        }
+
+        //instanciar um segundo objeto e fazer com o que o usuário preencha as informações para
+
+        public void pedirInformacaos()
+        {
+            Console.WriteLine("Por favor me diga seu Nome");
+            this.Nome = Console.ReadLine();
+            Console.WriteLine("Por favor me diga seu Email");
+            this.Email = Console.ReadLine();
+            Console.WriteLine("Por favor me diga seu Login");
+            this.Login = Console.ReadLine();
+            Console.WriteLine("Por favor me diga sua Senha");
+            this.Senha = Console.ReadLine();
+            Console.WriteLine("     ");
+            Console.WriteLine("   .  ");
+            Console.WriteLine("   .  ");
+            Console.WriteLine("   .  ");
+            Console.WriteLine("     ");
+            Console.WriteLine("Nome inserido: " + this.Nome);
+            Console.WriteLine("Email inserido: " + this.Email);
+            Console.WriteLine("Login inserido: " + this.Login);
+            Console.WriteLine("Senha inserida: " + this.Senha);
+        }
+    }
+
+}
+
+
